8334204d8cbe01f1686e9b0e1fd241b972452c56
FelixMeow <fm_matrix@hotmail.com>
2016-09-17 20:05:16 +0100

    betterNetworking

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 5f;
+
+	[SerializeField]
+	private float lookSensitivity = 3f;
+
+	private PlayerMotor motor;
+
+	void Start ()
+	{
+		motor = GetComponent<PlayerMotor>();
+	}
+
+	void PlayerMovement()
+	{
+		float xMove = Input.GetAxisRaw("Horizontal");
+		float zMove = Input.GetAxisRaw("Vertical");
+		Vector3 moveHorizontal = transform.right * xMove;
+		Vector3 moveVertical = transform.forward * zMove;
+		Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+		motor.Move(velocity);
+
+		float yRot = Input.GetAxisRaw("Mouse X");
+		Vector3 rotation = new Vector3 (0f, yRot, 0f) * lookSensitivity;
+		motor.Rotate(rotation);
+
+		float xRot = Input.GetAxisRaw("Mouse Y");
+		Vector3 camRotation = new Vector3 (xRot, 0f, 0f) * lookSensitivity;
+		motor.CamRotate(camRotation);
+
+
+
+
+	}
+
+	void Update()
+	{
+		PlayerMovement();
+	}
+
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+	[SerializeField]
+	private Camera cam;
+
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 rotation = Vector3.zero;
+	private Vector3 camRotation = Vector3.zero;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	public void Move(Vector3 vel)
+	{
+		velocity = vel;
+	}
+
+	public void Rotate(Vector3 rot)
+	{
+		rotation = rot;
+	}
+
+	public void CamRotate(Vector3 rot)
+	{
+		camRotation = rot;
+	}
+
+	void FixedUpdate ()
+	{
+		PerformMovement();
+		PerformRotation();
+	}
+
+	void PerformMovement()
+	{
+		if(velocity != Vector3.zero)
+		{
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+		}
+	}
+
+	void PerformRotation()
+	{
+		rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+		if(cam != null)
+		{
+			cam.transform.Rotate(-camRotation);
+		}
+	}
+
+}
