57cb754b59a909570d0939b59da2f2181c6c8408
FelixMeow <fm_matrix@hotmail.com>
2016-09-20 19:26:36 +0100

    host manager

---
+++ Assets/Scripts/HostManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostManager : NetworkBehaviour {
+
+	[SerializeField]
+	private uint roomSize = 5;
+
+	private string roomName;
+
+	private NetworkManager networkManager;
+
+	void Start ()
+	{
+		networkManager = NetworkManager.singleton;
+		if(networkManager.matchMaker == null)
+		{
+			networkManager.StartMatchMaker();
+		}
+	}
+
+	public void SetRoomName (string name)
+	{
+		roomName = name;
+	}
+
+	public void CreateRoom ()
+	{
+		if(roomName != "" && roomName != null)
+		{
+			Debug.Log("Creating room");	
+			networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", networkManager.OnMatchCreate);
+		}
+	}
+
+}
